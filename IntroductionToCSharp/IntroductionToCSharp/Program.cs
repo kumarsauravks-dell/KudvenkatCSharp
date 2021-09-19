8b8463246f4794c6636418b69aee16a59a113604
unknown <kumarsauravks.dell@gmail.com>
2021-09-19 15:49:11 +0530

    Initial Commit

---
+++ IntroductionToCSharp/IntroductionToCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter First Name");
+        string firstName=Console.ReadLine();
+        Console.WriteLine("Enter Last Name");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("Welcome " + firstName + " " + lastName);
+        Console.WriteLine("Welcome {0} {1}", firstName, lastName);
+        Console.ReadKey();
+    }
+}
