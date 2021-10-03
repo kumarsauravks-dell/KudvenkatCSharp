9744508259ec008c9ac593fc2d97e4d4f6e34c0b
unknown <kumarsauravks.dell@gmail.com>
2021-10-03 18:07:50 +0530

    c# notes till video 15

--- IntroductionToCSharp/IntroductionToCSharp/Program.cs
+++ IntroductionToCSharp/IntroductionToCSharp/Program.cs
@@ -3,27 +3,14 @@ class Program
 {
     public static void Main()
     {
-        int TotalCoffeeCost = 0;
-        Console.WriteLine("Which type of coffee do you want?");
-        Console.WriteLine("1-Small , 2-Medium , 3-Large");
-        int UserChoice=int.Parse(Console.ReadLine());
-        switch (UserChoice)
+        for(int i = 1; i <= 20; i++)
         {
-            case 1:
-                TotalCoffeeCost += 1;
-                break;
-            case 2:
-                TotalCoffeeCost += 2;
-                break;
-            case 3:
-                TotalCoffeeCost += 3;
-                break;
-            default:
-                Console.WriteLine("Your choice {0} is invalid.",UserChoice);
-                break;                
+            if (i % 2==1)
+            {
+                continue;
+            }
+            Console.WriteLine(i);
         }
-        Console.WriteLine("Thank you for shopping with us.");
-        Console.WriteLine("Your bill amount is : {0}", TotalCoffeeCost);
         Console.ReadKey();
     }
 }
