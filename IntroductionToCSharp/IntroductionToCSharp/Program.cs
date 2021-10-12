a4cf74b2bfa0523ed9163638f5658eecd2bdb315
unknown <kumarsauravks.dell@gmail.com>
2021-10-12 18:05:34 +0530

    Study notes till Video 30

--- IntroductionToCSharp/IntroductionToCSharp/Program.cs
+++ IntroductionToCSharp/IntroductionToCSharp/Program.cs
@@ -1,16 +1,35 @@
 ﻿using System;
+interface ICustomer1
+{
+    void Print1();
+}
+interface ICustomer2 : ICustomer1
+{
+    void Print2();
+}
+public class Customer : ICustomer2
+{
+    public void Print1()
+    {
+        Console.WriteLine("Print1 Method of ICustomer1");
+    }
+    public void Print2()
+    {
+        Console.WriteLine("Print2 Method of ICustomer2");
+    }
+}
 class Program
 {
     public static void Main()
     {
-        for(int i = 1; i <= 20; i++)
-        {
-            if (i % 2==1)
-            {
-                continue;
-            }
-            Console.WriteLine(i);
-        }
+        Customer C1 = new Customer();
+        C1.Print1();
+        C1.Print2();
+
+        ICustomer1 C2 = new Customer();
+        C2.Print1();
+
         Console.ReadKey();
+
     }
 }
